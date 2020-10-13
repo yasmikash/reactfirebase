cd971c872a4474a98bd796c70b2e2d2101b61b5c
yasmikash <yasmikash@gmail.com>
2020-10-13 17:13:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+// Example is creating a user with a password
+// Example code is taken from here >> https://firebase.google.com/docs/auth/web/password-auth
+
+function App({ firebase }) {
+  const createAccount = () => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword("yasmikash@gmail.com", "yasmikash")
+      .then((res) => {
+        // Whoo-hoo! User signed in!!
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Creating a new account</h2>
+      <button onClick={createAccount}>Create Account</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// We get the firebase object that is exposed in ./firebase/firebase.js and pass it as an global object
+import firebase from "./firebase/firebase";
+
+import App from "./App";
+
+ReactDOM.render(<App firebase={firebase} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
